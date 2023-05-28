d72341e77bd4f36770352e82b218990e08f89f6f
JoaoCosme <cosmejoaopedro@protonmail.com>
2023-05-28 15:22:24 -0300

    read_image

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 fn main() {
     println!("Iniciando leitura de arquivos");
+    let image = read_image();
+    println!("{} {}", image.width, image.height);
+}
+
+fn read_image() -> rawloader::RawImage {
     let file = "./src/images/scene-raw2.dng";
     let image = rawloader::decode_file(file).unwrap();
-    println!("{} {}", image.width, image.height);
+    image
 }
