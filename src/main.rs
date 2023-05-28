553094a90d1ed1be1dadc24f83fcf45f0276cbf7
JoaoCosme <cosmejoaopedro@protonmail.com>
2023-05-28 17:59:36 -0300

    demosaick em 50%

--- src/main.rs
+++ src/main.rs
@@ -20,54 +20,61 @@ fn main() {
     if let rawloader::RawImageData::Integer(data) = image.data {
         for x in 0..image.width as u32 {
             for y in 0..image.height as u32 {
-                let pixel = get_pixel_value(&data, x, y);
+                let width = image.width as u32;
+                let heigth = image.height as u32;
+                let pixel = get_pixel_value(&data, x, y, width, heigth);
                 //Para cada pixel verificar se é r g ou b
                 // Se for R interpolar verde e azul
                 // Se for G interpolar vermelho e azul
                 // Se for azul interpolar vermelho e verde
 
-                if x % 2 == 0 {
-                    if y % 2 == 0 {
+                let mut demosaic_pixel: Rgb<u8> = Rgb([255, 255, 255]);
+                if x % 2 == 0 || x == 0 {
+                    if y % 2 == 0 || y == 0 {
                         //Caso vermelho
-                        let green = get_pixel_value(&data, x, y + 1);
-                        let blue = get_pixel_value(&data, x + 1, y);
+                        let green = get_pixel_value(&data, x + 1, y, width, heigth);
+                        let blue = get_pixel_value(&data, x + 1, y + 1, width, heigth);
                         let red = pixel;
-                        let demosaic_pixel = Rgb([red, green, blue]);
-                        demosaic.put_pixel(x, y, demosaic_pixel);
+                        demosaic_pixel = Rgb([red, green, blue]);
                     } else {
                         //Caso verde
-                        let red = get_pixel_value(&data, x, y + 1);
-                        let blue = get_pixel_value(&data, x + 1, y);
+                        let red = get_pixel_value(&data, x + 1, y, width, heigth);
+                        let blue = get_pixel_value(&data, x + 1, y + 1, width, heigth);
                         let green = pixel;
                         let demosaic_pixel = Rgb([red, green, blue]);
                         demosaic.put_pixel(x, y, demosaic_pixel);
                     }
                 } else {
-                    if y % 2 == 0 {
+                    if y % 2 == 0 || y == 0 {
                         //Caso azul
-                        let green = get_pixel_value(&data, x, y + 1);
-                        let red = get_pixel_value(&data, x + 1, y);
+                        let green = get_pixel_value(&data, x + 1, y, width, heigth);
+                        let red = get_pixel_value(&data, x + 1, y + 1, width, heigth);
                         let blue = pixel;
                         let demosaic_pixel = Rgb([red, green, blue]);
                         demosaic.put_pixel(x, y, demosaic_pixel);
                     } else {
                         //Caso verde
-                        let blue: u8 = get_pixel_value(&data, x, y + 1);
-                        let red = get_pixel_value(&data, x + 1, y);
+                        let blue: u8 = get_pixel_value(&data, x + 1, y, width, heigth);
+                        let red = get_pixel_value(&data, x + 1, y + 1, width, heigth);
                         let green = pixel;
-                        let demosaic_pixel = Rgb([red, green, blue]);
-                        demosaic.put_pixel(x, y, demosaic_pixel);
+                        demosaic_pixel = Rgb([red, green, blue]);
                     }
                 }
+                demosaic.put_pixel(x, y, demosaic_pixel);
             }
         }
     }
     println!("Processamento completo! Iniciando salvamento...");
     demosaic.save("image.png").expect("Failed to save image");
+    println!("Salvamento completo com sucesso!");
 }
 
-fn get_pixel_value(data: &Vec<u16>, x: u32, y: u32) -> u8 {
-    let pixel = data[(x + 1 * y) as usize];
+fn get_pixel_value(data: &Vec<u16>, x: u32, y: u32, width: u32, height: u32) -> u8 {
+    let index = x * width + y;
+    if index >= width * height {
+        return 0;
+    }
+    let pixel = data[index as usize];
     let pixel = (pixel as f32 / MAX_12_BIT_VALUE * u8::MAX as f32) as u8;
     pixel
 }
