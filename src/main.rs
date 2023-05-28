51497f151df3394dc38d0d01ed4db3a75155e092
JoaoCosme <cosmejoaopedro@protonmail.com>
2023-05-28 15:45:11 -0300

    passing trough vector

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,17 @@
+use opencv::prelude::*;
+
 fn main() {
     println!("Iniciando leitura de arquivos");
     let image = read_image();
+    // let mut f = Mat::default();
     println!("{} {}", image.width, image.height);
+    if let rawloader::RawImageData::Integer(data) = image.data {
+        for x in 0..=image.width {
+            for y in 0..=image.height {
+                // println!("{}", data[x * y])
+            }
+        }
+    }
 }
 
 fn read_image() -> rawloader::RawImage {
