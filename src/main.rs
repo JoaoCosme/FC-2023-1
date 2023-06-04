023a17562e25e9e44c3ff5dc97a448bfb3f5c847
Joao Cosme <joao.pedro.cosme@sap.com>
2023-06-04 18:41:01 -0300

    improve image usage

--- src/main.rs
+++ src/main.rs
@@ -5,47 +5,75 @@ fn main() {
     println!("Iniciando leitura de arquivos.");
     let image = read_image();
     let demosaic = demosaick_image(image);
-    let white_balance = white_balance(&demosaic);
+    let white_balance_gray_world = white_balance(&demosaic);
+    let white_balance = white_balance_scaling(&demosaic);
     let gamma_encoded = gamma_correct_image(&white_balance);
     println!("Processamento completo! Iniciando salvamento...");
-    demosaic
-        .save("demosaick.png")
-        .expect("Failed to save image");
-    white_balance
-        .save("white_balance.png")
-        .expect("Failed to save image");
-    gamma_encoded
-        .save("gamma_encoded.png")
-        .expect("Failed to save image");
+    save_image(&demosaic, "demosaick.png");
+    save_image(&white_balance, "white_balance.png");
+    save_image(&white_balance_gray_world, "white_balance_gray_world.png");
+    save_image(&gamma_encoded, "gamma_encoded.png");
     println!("Salvamento completo com sucesso!");
 }
 
-fn gamma_correct_image(image: &ImageBuffer<Rgb<u8>, Vec<u8>>) -> ImageBuffer<Rgb<u8>, Vec<u8>> {
+fn save_image(image: &ImageBuffer<Rgb<f32>, Vec<f32>>, file_name: &str) {
+    let (width, height) = image.dimensions();
+    let mut output_image: ImageBuffer<image::Rgb<u8>, Vec<u8>> = ImageBuffer::new(width, height);
+
+    for (x, y, pixel) in image.enumerate_pixels() {
+        let r = (pixel[0] * 255.0).round() as u8;
+        let g = (pixel[1] * 255.0).round() as u8;
+        let b = (pixel[2] * 255.0).round() as u8;
+
+        output_image.put_pixel(x, y, image::Rgb([r, g, b]));
+    }
+
+    output_image.save(file_name).expect("Should save image");
+}
+fn gamma_correct_image(image: &ImageBuffer<Rgb<f32>, Vec<f32>>) -> ImageBuffer<Rgb<f32>, Vec<f32>> {
     let mut gamma_corrected_image = ImageBuffer::new(image.width(), image.height());
-    let apply_gamma = |value: u8| -> u8 { (value as f32).powf(1.0 / 2.2) as u8 };
-    for x in 0..image.width() {
-        for y in 0..image.height() {
-            let pixel = image.get_pixel(x, y).0;
-            let red = apply_gamma(pixel[0]);
-            let green = apply_gamma(pixel[1]);
-            let blue = apply_gamma(pixel[2]);
-            gamma_corrected_image.put_pixel(x, y, Rgb([red, green, blue]));
-        }
+    let apply_gamma = |value: f32| -> f32 { (value as f32).powf(1.0 / 2.2) };
+    for (x, y, pixel) in image.enumerate_pixels() {
+        let red = apply_gamma(pixel[0]);
+        let green = apply_gamma(pixel[1]);
+        let blue = apply_gamma(pixel[2]);
+        gamma_corrected_image.put_pixel(x, y, Rgb([red, green, blue]));
     }
     gamma_corrected_image
 }
 
-fn white_balance(image: &ImageBuffer<Rgb<u8>, Vec<u8>>) -> ImageBuffer<Rgb<u8>, Vec<u8>> {
+fn white_balance_scaling(
+    image: &ImageBuffer<Rgb<f32>, Vec<f32>>,
+) -> ImageBuffer<Rgb<f32>, Vec<f32>> {
+    let red_adjust = 255f32 / 170f32;
+    let green_adjust = 255f32 / 231f32;
+    let blue_adjust = 255f32 / 141f32;
+
+    let mut white_balanced_image = ImageBuffer::new(image.width(), image.height());
+
+    for (x, y, pixel) in image.enumerate_pixels() {
+        let red = pixel[0] * red_adjust;
+        let green = pixel[1] * green_adjust;
+        let blue = pixel[2] * blue_adjust;
+
+        white_balanced_image.put_pixel(x, y, Rgb([red, green, blue]));
+    }
+
+    // Computar os ganhos alfa e beta a partir disso
+    // Multiplicar R pelo ganho alfa
+    // Multiplicar B pelo ganho beta
+    white_balanced_image
+}
+
+fn white_balance(image: &ImageBuffer<Rgb<f32>, Vec<f32>>) -> ImageBuffer<Rgb<f32>, Vec<f32>> {
     // Computar a media de R G e B ao longo da imagem
     let mut red_avg: f32 = 0f32;
     let mut green_avg: f32 = 0f32;
     let mut blue_avg: f32 = 0f32;
-    for x in 0..image.width() {
-        for y in 0..image.height() {
-            red_avg += image.get_pixel(x, y).0[0] as f32;
-            green_avg += image.get_pixel(x, y).0[1] as f32;
-            blue_avg += image.get_pixel(x, y).0[2] as f32;
-        }
+    for (x, y, pixel) in image.enumerate_pixels() {
+        red_avg += pixel[0] as f32;
+        green_avg += pixel[1] as f32;
+        blue_avg += pixel[2] as f32;
     }
     red_avg = red_avg / (image.width() * image.height()) as f32;
     green_avg = green_avg / (image.width() * image.height()) as f32;
@@ -58,22 +86,12 @@ fn white_balance(image: &ImageBuffer<Rgb<u8>, Vec<u8>>) -> ImageBuffer<Rgb<u8>, 
 
     let mut white_balanced_image = ImageBuffer::new(image.width(), image.height());
 
-    for x in 0..image.width() {
-        for y in 0..image.height() {
-            let red = image.get_pixel(x, y).0[0] as f32 * alfa;
-            let green = image.get_pixel(x, y).0[1];
-            let blue = image.get_pixel(x, y).0[2] as f32 * beta;
+    for (x, y, pixel) in image.enumerate_pixels() {
+        let red = pixel[0] * alfa;
+        let green = pixel[1];
+        let blue = pixel[2] * beta;
 
-            white_balanced_image.put_pixel(
-                x,
-                y,
-                Rgb([
-                    normalize_pixel_val(red as u16),
-                    normalize_pixel_val(green as u16),
-                    normalize_pixel_val(blue as u16),
-                ]),
-            );
-        }
+        white_balanced_image.put_pixel(x, y, Rgb([red, green, blue]));
     }
 
     // Computar os ganhos alfa e beta a partir disso
@@ -82,7 +100,7 @@ fn white_balance(image: &ImageBuffer<Rgb<u8>, Vec<u8>>) -> ImageBuffer<Rgb<u8>, 
     white_balanced_image
 }
 
-fn demosaick_image(image: rawloader::RawImage) -> ImageBuffer<Rgb<u8>, Vec<u8>> {
+fn demosaick_image(image: rawloader::RawImage) -> ImageBuffer<Rgb<f32>, Vec<f32>> {
     let mut demosaic = ImageBuffer::new(image.width as u32, image.height as u32);
 
     println!(
@@ -104,6 +122,7 @@ fn demosaick_image(image: rawloader::RawImage) -> ImageBuffer<Rgb<u8>, Vec<u8>> 
 
     if let rawloader::RawImageData::Integer(data) = image.data {
         let get_pixel_value = make_get_pixel(&data, image.width as u32, image.height as u32);
+
         let get_avg_horizontal_and_vertical = |x: u32, y: u32| -> u16 {
             (get_pixel_value(x + 1, y)
                 + get_pixel_value(x.saturating_sub(1), y)
@@ -113,16 +132,16 @@ fn demosaick_image(image: rawloader::RawImage) -> ImageBuffer<Rgb<u8>, Vec<u8>> 
         };
         let get_avg_horizontal = |x: u32, y: u32| -> u16 {
             (get_pixel_value(x + 1, y)
-                + get_pixel_value(x + 2, y)
+                + get_pixel_value(x + 3, y)
                 + get_pixel_value(x.saturating_sub(1), y)
-                + get_pixel_value(x.saturating_sub(2), y))
+                + get_pixel_value(x.saturating_sub(3), y))
                 / 4
         };
         let get_avg_vertical = |x: u32, y: u32| -> u16 {
             (get_pixel_value(x, y + 1)
-                + get_pixel_value(x, y + 2)
+                + get_pixel_value(x, y + 3)
                 + get_pixel_value(x, y.saturating_sub(1))
-                + get_pixel_value(x, y.saturating_sub(2)))
+                + get_pixel_value(x, y.saturating_sub(3)))
                 / 4
         };
         let get_avg_diagonally = |x: u32, y: u32| -> u16 {
@@ -145,8 +164,8 @@ fn demosaick_image(image: rawloader::RawImage) -> ImageBuffer<Rgb<u8>, Vec<u8>> 
                 let red;
                 let green;
                 let blue;
-                if x % 2 != 0 || x == 0 {
-                    if y % 2 != 0 || y == 0 {
+                if x % 2 == 0 || x == 0 {
+                    if y % 2 == 0 || y == 0 {
                         //Caso vermelho
                         green = get_avg_horizontal_and_vertical(x, y);
                         blue = get_avg_diagonally(x, y);
@@ -159,15 +178,15 @@ fn demosaick_image(image: rawloader::RawImage) -> ImageBuffer<Rgb<u8>, Vec<u8>> 
                     }
                 } else {
                     if y % 2 == 0 || y == 0 {
-                        //Caso azul
-                        green = get_avg_horizontal_and_vertical(x, y);
-                        red = get_avg_diagonally(x, y);
-                        blue = pixel;
-                    } else {
                         //Caso verde
                         blue = get_avg_horizontal(x, y);
                         red = get_avg_vertical(x, y);
                         green = pixel;
+                    } else {
+                        //Caso azul
+                        green = get_avg_horizontal_and_vertical(x, y);
+                        red = get_avg_diagonally(x, y);
+                        blue = pixel;
                     }
                 }
                 demosaic_pixel = Rgb([
@@ -186,14 +205,14 @@ fn make_get_pixel(data: &Vec<u16>, width: u32, height: u32) -> impl Fn(u32, u32)
     move |x: u32, y: u32| -> u16 {
         let index = x + y * width;
         if index >= width * height || index <= 0 {
-            return 255;
+            return 0;
         }
         data[index as usize]
     }
 }
 
-fn normalize_pixel_val(pixel: u16) -> u8 {
-    (pixel as f32 / MAX_12_BIT_VALUE * u8::MAX as f32) as u8
+fn normalize_pixel_val(pixel: u16) -> f32 {
+    (pixel as f32 / MAX_12_BIT_VALUE)
 }
 fn read_image() -> rawloader::RawImage {
     let file = "./src/images/scene-raw.dng";
